1e6e2a005f807b0361d90ef55abf2f20230e8065
Ryanzhang1 <253914387@qq.com>
2022-08-12 17:46:34 +0800

    2022.8.12 fix dcatenate in testing null input

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,6 +82,9 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A==null){
+            return B;
+        }
         IntList node=A;
         while(node.rest!=null){
             node=node.rest;
