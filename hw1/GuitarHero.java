d2dfee78efaa476169bae1b18704947a62939e27
Ryanzhang1 <253914387@qq.com>
2022-08-30 00:25:13 +0800

    2022.8.29 update2

--- hw1/GuitarHero.java
+++ hw1/GuitarHero.java
@@ -1,14 +1,14 @@
 public class GuitarHero {
     private static final double CONCERT_A = 440.0;
-    private static final String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
-    private static final int totalNotes = 37;
+    private static final String keyBoard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+    private static final int total = 37;
 
     private static double ithConcert(int i) {
         return CONCERT_A * Math.pow(2, (i - 24) / 12);
     }
 
     public static void main(String[] args) {
-        synthesizer.GuitarString[] gs = new synthesizer.GuitarString[totalNotes];
+        synthesizer.GuitarString[] gs = new synthesizer.GuitarString[total];
         for (int i = 0; i < 37; i++) {
             gs[i] = new synthesizer.GuitarString(ithConcert(i));
         }
@@ -16,9 +16,9 @@ public class GuitarHero {
         while (true) {
             if (StdDraw.hasNextKeyTyped()) {
                 char key = StdDraw.nextKeyTyped();
-                int index = keyboard.indexOf(key);
+                int index = keyBoard.indexOf(key);
                 if (index < 0) {
-                    System.out.println("please enter valid key : " + keyboard);
+                    System.out.println("please enter valid key : " + keyBoard);
                     continue;
                 }
                 gs[index].pluck();
--- hw1/synthesizer/GuitarString.java
+++ hw1/synthesizer/GuitarString.java
@@ -1,4 +1,4 @@
-// TODO: Make sure to make this class a part of the synthesizer package
+
 package synthesizer;
 
 import synthesizer.ArrayRingBuffer;
@@ -18,10 +18,7 @@ public class GuitarString {
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
-        // TODO: Create a buffer with capacity = SR / frequency. You'll need to
-        //       cast the result of this divsion operation into an int. For better
-        //       accuracy, use the Math.round() function before casting.
-        //       Your buffer should be initially filled with zeros.
+
         int capacity = (int) Math.round(SR / frequency);
         buffer = new ArrayRingBuffer<Double>(capacity);
         for (int i = 0; i < capacity; i++) {
@@ -32,11 +29,7 @@ public class GuitarString {
 
     /* Pluck the guitar string by replacing the buffer with white noise. */
     public void pluck() {
-        // TODO: Dequeue everything in the buffer, and replace it with random numbers
-        //       between -0.5 and 0.5. You can get such a number by using:
-        //       double r = Math.random() - 0.5;
-        //
-        //       Make sure that your random numbers are different from each other.
+
         for (int i = 0; i < buffer.capacity(); i++) {
             buffer.dequeue();
             buffer.enqueue(Math.random() - 0.5);
@@ -47,9 +40,7 @@ public class GuitarString {
      * the Karplus-Strong algorithm.
      */
     public void tic() {
-        // TODO: Dequeue the front sample and enqueue a new sample that is
-        //       the average of the two multiplied by the DECAY factor.
-        //       Do not call StdAudio.play().
+
         double frontNumber = buffer.dequeue();
         double average = (frontNumber + buffer.peek()) * DECAY * 0.5;
         buffer.enqueue(average);
@@ -57,7 +48,7 @@ public class GuitarString {
 
     /* Return the double at the front of the buffer. */
     public double sample() {
-        // TODO: Return the correct thing.
+
         return buffer.peek();
     }
 }
